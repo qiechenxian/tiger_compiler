f1d322879703331b04b5bc84e329b7f6070f5419
chuyaoxiang <947384932@qq.com>
2020-08-01 11:54:17 +0800

    drop protect reg before call lib

--- codegen.cpp
+++ codegen.cpp
@@ -361,28 +361,28 @@ static void munchStm(T_stm s) {
                         //TODO 函数调用测试能否正确传参
                         sprintf(inst, "\tbl      %s\n", Temp_labelString(lab));
                         emit(AS_Oper(inst, L(F_LR(), calldefs), l, AS_Targets(Temp_LabelList(lab, NULL))));
-                        if (special_tag == true) {
-                            count_func_param = count_func_param - 2;
-                            if (count_func_param >= 0) {
-                                sprintf(inst_r0, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                                emit(AS_Oper(inst_r0, L(F_R0(), NULL), L(F_SP(), NULL), NULL));
-                            }
-                            count_func_param--;
-                            if (count_func_param >= 0) {
-                                sprintf(inst_r1, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                                emit(AS_Oper(inst_r1, L(F_R1(), NULL), L(F_SP(), NULL), NULL));
-                            }
-                            count_func_param--;
-                            if (count_func_param >= 0) {
-                                sprintf(inst_r2, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                                emit(AS_Oper(inst_r2, L(F_R2(), NULL), L(F_SP(), NULL), NULL));
-                            }
-                            count_func_param--;
-                            if (count_func_param >= 0) {
-                                sprintf(inst_r3, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                                emit(AS_Oper(inst_r3, L(F_R3(), NULL), L(F_SP(), NULL), NULL));
-                            }
-                        }
+                       // if (special_tag == true) {
+                       //     count_func_param = count_func_param - 2;
+                       //     if (count_func_param >= 0) {
+                       //         sprintf(inst_r0, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                       //         emit(AS_Oper(inst_r0, L(F_R0(), NULL), L(F_SP(), NULL), NULL));
+                       //     }
+                       //     count_func_param--;
+                       //     if (count_func_param >= 0) {
+                       //         sprintf(inst_r1, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                       //         emit(AS_Oper(inst_r1, L(F_R1(), NULL), L(F_SP(), NULL), NULL));
+                       //     }
+                       //     count_func_param--;
+                       //     if (count_func_param >= 0) {
+                       //         sprintf(inst_r2, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                       //         emit(AS_Oper(inst_r2, L(F_R2(), NULL), L(F_SP(), NULL), NULL));
+                       //     }
+                       //     count_func_param--;
+                       //     if (count_func_param >= 0) {
+                       //         sprintf(inst_r3, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                       //        emit(AS_Oper(inst_r3, L(F_R3(), NULL), L(F_SP(), NULL), NULL));
+                       //     }
+                       // }
                         sprintf(inst2, "\tmov     'd0, 's0\n");
                         emit(AS_Move(inst2, L(t, NULL), L(F_RV(), NULL)));
                     } else {
@@ -456,31 +456,31 @@ static void munchStm(T_stm s) {
                     // 函数调用？
                     sprintf(inst, "\tbl      %s\n", Temp_labelString(lab));
                     emit(AS_Oper(inst, calldefs, l, AS_Targets(Temp_LabelList(lab, NULL))));
-                    if (special_tag == true) {
-                        count_func_param = count_func_param - 2;
-                        if (count_func_param >= 0) {
-                            sprintf(inst_r0, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                            emit(AS_Oper(inst_r0, L(F_R0(), NULL), L(F_SP(), NULL), NULL));
-                        }
+                    //if (special_tag == true) {
+                    //    count_func_param = count_func_param - 2;
+                    //    if (count_func_param >= 0) {
+                    //        sprintf(inst_r0, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                    //        emit(AS_Oper(inst_r0, L(F_R0(), NULL), L(F_SP(), NULL), NULL));
+                    //    }
 
-                        count_func_param--;
-                        if (count_func_param >= 0) {
-                            sprintf(inst_r1, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                            emit(AS_Oper(inst_r1, L(F_R1(), NULL), L(F_SP(), NULL), NULL));
-                        }
+                    //   count_func_param--;
+                    //    if (count_func_param >= 0) {
+                    //        sprintf(inst_r1, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                    //        emit(AS_Oper(inst_r1, L(F_R1(), NULL), L(F_SP(), NULL), NULL));
+                    //    }
 
-                        count_func_param--;
-                        if (count_func_param >= 0) {
-                            sprintf(inst_r2, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                            emit(AS_Oper(inst_r2, L(F_R2(), NULL), L(F_SP(), NULL), NULL));
-                        }
+                    //    count_func_param--;
+                    //    if (count_func_param >= 0) {
+                    //        sprintf(inst_r2, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                    //        emit(AS_Oper(inst_r2, L(F_R2(), NULL), L(F_SP(), NULL), NULL));
+                    //    }
 
-                        count_func_param--;
-                        if (count_func_param >= 0) {
-                            sprintf(inst_r3, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
-                            emit(AS_Oper(inst_r3, L(F_R3(), NULL), L(F_SP(), NULL), NULL));
-                        }
-                    }
+                    //    count_func_param--;
+                    //    if (count_func_param >= 0) {
+                    //        sprintf(inst_r3, "\tldr     'd0, ['s0, #%d]\n", count_func_param * get_word_size());
+                    //        emit(AS_Oper(inst_r3, L(F_R3(), NULL), L(F_SP(), NULL), NULL));
+                    //    }
+                    //}
                 } else {
                     /* EXP(CALL(e,args)) */
                     T_exp e1 = call->u.CALL.fun;
@@ -611,35 +611,35 @@ static Temp_tempList munchArgs(bool tag, int i, T_expList args)
         } else if (args_count >= 0) {
             switch (count_func_param - 1) {
                 case 0: {
-                    sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r0  s1预着色为sp
-                    emit(AS_Oper(str, NULL, L(F_R0(), L(F_SP(), NULL)), NULL));
+                    //sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r0  s1预着色为sp
+                    //emit(AS_Oper(str, NULL, L(F_R0(), L(F_SP(), NULL)), NULL));
 
-                    sprintf(inst, "\tldr     'd0, ['s0]\n");
-                    emit(AS_Oper(inst, L(F_R0(), NULL), L(r, NULL), NULL));
+                    sprintf(inst, "\tmov     'd0, 's0\n");
+                    emit(AS_Move(inst, L(F_R0(), NULL), L(r, NULL)));
                     break;
                 }
                 case 1: {
-                    sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r1  s1预着色为sp
-                    emit(AS_Oper(str, NULL, L(F_R1(), L(F_SP(), NULL)), NULL));
+                    //sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r1  s1预着色为sp
+                    //emit(AS_Oper(str, NULL, L(F_R1(), L(F_SP(), NULL)), NULL));
 
-                    sprintf(inst, "\tldr     'd0, ['s0]\n");
-                    emit(AS_Oper(inst, L(F_R1(), NULL), L(r, NULL), NULL));
+                    sprintf(inst, "\tmov     'd0, 's0\n");
+                    emit(AS_Move(inst, L(F_R1(), NULL), L(r, NULL)));
                     break;
                 }
                 case 2: {
-                    sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r2  s1预着色为sp
-                    emit(AS_Oper(str, NULL, L(F_R2(), L(F_SP(), NULL)), NULL));
+                    //sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r2  s1预着色为sp
+                    //emit(AS_Oper(str, NULL, L(F_R2(), L(F_SP(), NULL)), NULL));
 
-                    sprintf(inst, "\tldr     'd0, ['s0]\n");
-                    emit(AS_Oper(inst, L(F_R2(), NULL), L(r, NULL), NULL));
+                    sprintf(inst, "\tmov     'd0, 's0\n");
+                    emit(AS_Move(inst, L(F_R2(), NULL), L(r, NULL)));
                     break;
                 }
                 case 3: {
-                    sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r3  s1预着色为sp
-                    emit(AS_Oper(str, NULL, L(F_R3(), L(F_SP(), NULL)), NULL));
+                    //sprintf(str, "\tstr     's0, ['s1, #%d]\n", (--args_count) * get_word_size());//s0预着色为r3  s1预着色为sp
+                    //emit(AS_Oper(str, NULL, L(F_R3(), L(F_SP(), NULL)), NULL));
 
-                    sprintf(inst, "\tldr     'd0, ['s0]\n");
-                    emit(AS_Oper(inst, L(F_R3(), NULL), L(r, NULL), NULL));
+                    sprintf(inst, "\tmov     'd0, 's0\n");
+                    emit(AS_Move(inst, L(F_R3(), NULL), L(r, NULL)));
                     break;
                 }
             }
